cc2ac02bd8c63878bed7c921a50c1afea74e7f50
aluno <aluno@10.61.7.27>
2026-04-29 11:17:15 -0300

    commit verificação de cpf

--- src/controller/CadastroUsuarioController.java
+++ src/controller/CadastroUsuarioController.java
@@ -24,42 +24,41 @@ public CadastroUsuarioController(TelaCadastro view, UsuarioDAO model, Navegador 
 
 
 	this.view.cadastrar(e -> {
-		String nome = view.getNome();
-		String cpf= view.getCPF();
-		String senha = view.getSenha();
+		String nome = view.getNome().trim();
+		String cpf= view.getCPF().trim();
+		String senha = view.getSenha().trim();
 		
-		if(!nome.equals("") &&!cpf.equals("") && !senha.equals("")) {
-			
+		if (nome.isEmpty() || cpf.isEmpty() || senha.isEmpty()) {
+			this.view.exibirMensagem("Erro", "Preencha todos os campos!", 0);
+		}
+		else if (nome.length() < 3) {
+			this.view.exibirMensagem("Erro", "Nome muito curto!", 0);
+		}
+
+		else if (senha.length() < 4) {
+			this.view.exibirMensagem("Erro", "Senha muito curta!", 0);
+		}
+		else if (!cpf.matches("\\d{11}")) {
+			this.view.exibirMensagem("Erro", "CPF inválido!", 0);
+		}
+		else {
 			
 			Usuario.TipoUsuario tipo = view.isAdministrador()
                     ? Usuario.TipoUsuario.ADMIN
                     : Usuario.TipoUsuario.CLIENTE;
 
-			
-			
             Usuario usuario = new Usuario(nome, cpf, senha, tipo);
-
-             this.model.adicionarUsuario(usuario);
-
+            this.model.adicionarUsuario(usuario);
         	this.view.limparFormulario();
 			this.view.exibirMensagem("Sucesso", " Usuario Salvo", 1);
 		}
 		
-		else {
-			this.view.exibirMensagem("Erro", "Preencha todos os campos!", 0);
-		}
 		
 	});
 	
 	view.irParaLogin(e -> {
 		navegador.navegarPara("LOGIN");
 	});
-
-
-
-            this.view.limparFormulario();
-
-
 
 }
 }
